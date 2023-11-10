9b52720b8fb0613fd0ad830f3700bb723f45fc5e
17zhtang <1874906277@qq.com>
2023-11-10 14:22:54 +0800

    跨域改用过滤器处理

--- src/main/java/com/hltn/test/config/WebMVCConfig.java
+++ src/main/java/com/hltn/test/config/WebMVCConfig.java
@@ -2,7 +2,12 @@ package com.hltn.test.config;
 
 import com.hltn.test.interceptors.LoginProtectedInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,17 +17,34 @@ public class WebMVCConfig implements WebMvcConfigurer {
     private LoginProtectedInterceptor loginProtectedInterceptor;
 
     //跨域配置addCorsMappings  允许所有请求跨域
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                //是否发送Cookie
-                .allowCredentials(true)
-                //放行哪些原始域
-                .allowedOriginPatterns("*")
-                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
-                .allowedHeaders("*")
-                .exposedHeaders("*");
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                //是否发送Cookie
+//                .allowCredentials(true)
+//                //放行哪些原始域
+//                .allowedOriginPatterns("*")
+//                .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+//                .allowedHeaders("*")
+//                .exposedHeaders("*");
+//    }
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // 设置允许跨域请求的域名
+        config.addAllowedOrigin("*");
+        // 是否允许证书 不再默认开启
+        // config.setAllowCredentials(true);
+        // 设置允许的方法
+        config.addAllowedMethod("*");
+        // 允许任何头
+        config.addAllowedHeader("*");
+        config.addExposedHeader("token");
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+        return new CorsFilter(configSource);
     }
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
--- src/main/java/com/hltn/test/interceptors/LoginProtectedInterceptor.java
+++ src/main/java/com/hltn/test/interceptors/LoginProtectedInterceptor.java
@@ -1,6 +1,7 @@
 package com.hltn.test.interceptors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hltn.test.exception.AuthException;
 import com.hltn.test.service.UserService;
 import com.hltn.test.utils.JwtHelper;
 import com.hltn.test.utils.Result;
@@ -25,8 +26,10 @@ public class LoginProtectedInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("进入登录状态拦截器");
-        String token = request.getHeader("token");
+        String token = request.getHeader("Token");
         Long user_id = jwtHelper.getUserId(token);
+        log.info("用户id"+user_id);
+        log.info("请求token"+token);
         String userToken = userService.getToken(user_id); //数据库中的token
         boolean equal = userToken.equals(token);
         boolean expiration = jwtHelper.isExpiration(token);
@@ -38,6 +41,7 @@ public class LoginProtectedInterceptor implements HandlerInterceptor {
         ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(result);
         response.getWriter().print(json);
-        return false;
+        throw new AuthException();  //认证失败，抛异常
+//        return false;
     }
 }
